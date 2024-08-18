035a533406f47949327c0d622ae2bbe119a932c7
stdelpratobb <sdp@bb.vision>
2024-08-17 23:46:55 -0300

    updated version 0.5.2 > 0.5.5

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-APP_TITLE = "Whisper Transcription 0.5.2"
+APP_TITLE = "Whisper Transcription 0.5.5"
 MODEL_ID = "openai/whisper-large-v3"
 SUPPORTED_AUDIO_FORMATS = ('.mp3', '.wav', '.m4a', '.flac', '.ogg')
