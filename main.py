d6f238ab576060dbf226092eaf62885ea87386c9
stdelpratobb <sdp@bb.vision>
2024-08-27 19:45:11 -0300

    fixes + optimizacion

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ os.environ['HF_HUB_DISABLE_SYMLINKS_WARNING'] = '1'
 
 import sys
 from PyQt6.QtWidgets import QApplication
-from gui.main_window import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    from gui.main_window import MainWindow
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
